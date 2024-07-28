ba7622ce8a71ac27aca51410fedd2c8d63933830
Van Ha <halevan0592@gmail.com>
2024-07-28 21:47:41 +0700

    first commit

---
+++ src/features/books/booksSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  data: [],
+  page: 1,
+  total: 10,
+  bookDetail: undefined
+}
+
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    updateBooks: (state, action) => {
+      state.data = action.payload
+    },
+    updatePage: (state, action) => {
+      state.page = action.payload
+    },
+    updateBookDetail: (state, action) => {
+      state.bookDetail = action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { updateBooks, updatePage, updateBookDetail } = booksSlice.actions
+
+export default booksSlice.reducer
---
+++ src/pages/HomePage.js
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import { ClipLoader } from "react-spinners";
+import { useNavigate } from "react-router-dom";
+import PaginationBar from "../components/PaginationBar";
+import SearchForm from "../components/SearchForm";
+import api from "../apiService";
+import { FormProvider } from "../form";
+import { useForm } from "react-hook-form";
+import { Container, Alert, Box, Card, Stack, CardMedia, CardActionArea, Typography, CardContent } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { updateBooks } from "../features/books/booksSlice";
+
+
+
+const BACKEND_API = process.env.REACT_APP_BACKEND_API;
+
+const HomePage = () => {
+  const books = useSelector((state) => state.books.data)
+  const totalPage = useSelector((state) => state.books.total)
+  const pageNum = useSelector((state) => state.books.page)
+  const dispatch = useDispatch()
+  const limit = 10;
+
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate()
+  const handleClickBook = (bookId) => {
+    navigate(`/books/${bookId}`);
+  };
+
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        let url = `/books?_page=${pageNum}&_limit=${limit}`;
+        if (query) url += `&q=${query}`;
+        const res = await api.get(url);
+        const data = res.data
+        dispatch(updateBooks(data))
+        setErrorMessage("");
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [pageNum, limit, query]);
+  //--------------form
+  const defaultValues = {
+    searchQuery: ""
+  };
+  const methods = useForm({
+    defaultValues,
+  });
+  const { handleSubmit } = methods;
+  const onSubmit = (data) => {
+    setQuery(data.searchQuery);
+  };
+  return (
+    <Container>
+      <Stack sx={{ display: "flex", alignItems: "center", m: "2rem" }}>
+        <Typography variant="h3" sx={{ textAlign: "center" }}>Book Store</Typography>
+        {errorMessage && <Alert severity="danger">{errorMessage}</Alert>}
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+          <Stack
+            spacing={2}
+            direction={{ xs: "column", sm: "row" }}
+            alignItems={{ sm: "center" }}
+            justifyContent="space-between"
+            mb={2}
+          >
+            <SearchForm />
+          </Stack>
+        </FormProvider>
+        <PaginationBar
+          pageNum={pageNum}
+          totalPageNum={totalPage}
+        />
+      </Stack>
+      <div>
+        {loading ? (
+          <Box sx={{ textAlign: "center", color: "primary.main" }} >
+            <ClipLoader color="inherit" size={150} loading={true} />
+          </Box>
+        ) : (
+          <Stack direction="row" spacing={2} justifyContent="space-around" flexWrap="wrap">
+            {books.map((book) => (
+              <Card
+                key={book.id} onClick={() => handleClickBook(book.id)}
+                sx={{
+                  width: "12rem",
+                  height: "27rem",
+                  marginBottom: "2rem",
+                }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={`${BACKEND_API}/${book.imageLink}`}
+                    alt={`${book.title}`}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {`${book.title}`}
+                    </Typography>
+
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
+          </Stack>
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
